542dfdf6af6b1bde38c8b1eece9af42b3120037c
John Doe <ohndoe@example.com>
2022-11-25 11:19:07 +0600

    commit

---
+++ lib/widget/round_text_field_pass.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:tasks_app/const/color.dart';
+import 'package:tasks_app/widget/text_field.dart';
+
+class RoundTextFieldPass extends StatelessWidget {
+  final String hintText;
+  final IconData icon;
+  final ValueChanged<String> onChanged;
+  const RoundTextFieldPass(
+      {Key? key,
+      required this.hintText,
+      required this.icon,
+      required this.onChanged})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldCon(
+      child: TextField(
+        onChanged: onChanged,
+        obscureText: true,
+        decoration: InputDecoration(
+            icon: Icon(
+              icon,
+              color: scaffoldColor,
+            ),
+            hintText: hintText,
+            hintStyle: TextStyle(color: scaffoldColor),
+            border: InputBorder.none),
+      ),
+    );
+  }
+}
